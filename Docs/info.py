43d7164de190b3bb48d4eb938672732b155f6639
Manav56 <71647395+Manav-56@users.noreply.github.com>
2022-12-08 14:11:36 +0100

    Create info.py

---
+++ Docs/info.py
@@ -0,0 +1,21 @@
+# Clean Code Cheat Sheet
+# which i have been follow for this application develpoment
+
+# 1) used either '' or "" throught out the application develpoment.
+# I used '' for this application.
+
+# 2) avoid multiple loops for within the one function
+
+# 3) use Constant for avoid re-write same code
+
+# 4) Naming Conventions
+# variable should be lower_case
+# CONSTANTS should be IN_ALL_CAPS_USE_UNDERSCORE_FOR_SPACE
+# functions should be verbs_in_lower_case
+
+# 5) Give meaningful name to variables and functions
+
+# 6) Give meaningful comment whenever possible, it allows anyones to understand your better.
+
+# 7) Push code on the regular basis to GitHUb with meaningful message while  commiting code. It allows you see in the future, what commits you have done it.  
+
