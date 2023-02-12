c786dfe9cf02b8202e81956d176ab768606c8775
Manav56 <71647395+Manav-56@users.noreply.github.com>
2023-02-12 15:07:28 +0100

    add sheet file

---
+++ files/sheet.py
@@ -0,0 +1,23 @@
+# Clean Code Cheat Sheet
+
+# 1) Be consistent
+# Used either ' ' or " " throught out the application develpoment. I used " " for this application
+
+# 2) Naming Conventions
+# variable should be nouns_in_lower_case
+# CONSTANTS should be NOUNS_IN_ALL_CAPS_WITH_UNDERSCORES
+# functions should be verbs_in_lower_case
+# classes should be CamelCase
+
+# 3) Add relavant comment, just for code more readlable.
+
+# 4) Add meaningful name for funcation,class, and varaibles.
+
+# 5) Use constants to avoid repeating the same code again and again.
+
+# 6) Create simple test cases for better understanding as compared to one complicated test case.
+
+# 7) Opening files in a block with "with" has the advantage that the file gets automatically closed when leaving the code block.
+
+# 8) Use Library
+# whenever there is a library available; that can make your project easier.
